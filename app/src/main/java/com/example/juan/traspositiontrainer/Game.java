f8650ce018933872351f8d74cf3af9a4113015cc
Juan <juanf03@gmail.com>
2015-07-24 02:55:32 -0300

    -Agregados todos los intents(redirecciona todos los botones a su Activity correspondiente) -Agregadas todas las actionBars a todos los activities así se puede tocar la configuración desde cualquier pantalla sin tener que volver al menú para configurar las SharedPreferences -Agregada la pantalla de Faq -Agregada la pantalla de Stats -Agregada la pantalla de juego (Game.class) -Agregada la funcionalidad de saber que botón presionó el usuario para capturar desde la pantalla de game si buscar en la base de datos acordes, notas o progresiones de acordes según lo que venga en el String  de GetExtras "ButtonPressed"

---
+++ app/src/main/java/com/example/juan/traspositiontrainer/Game.java
@@ -0,0 +1,68 @@
+package com.example.juan.traspositiontrainer;
+
+import android.app.DialogFragment;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class Game extends ActionBarActivity {
+
+String message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        Intent intent = getIntent();
+        Bundle var = getIntent().getExtras();
+        if(var != null){
+            message = var.getString("ButtonPressed");
+        }
+        //String message = intent.getStringExtra("ButtonPressed");
+        TextView textView =(TextView) findViewById(R.id.intent);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+
+            DialogFragment myFragment = new MyDialogFragment();
+
+            myFragment.show(getFragmentManager(), "theDialog");
+
+            return true;
+        }else if (id== R.id.exit_app){
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
+}
--- app/src/main/java/com/example/juan/traspositiontrainer/MainActivity.java
+++ app/src/main/java/com/example/juan/traspositiontrainer/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.juan.traspositiontrainer;
 
 import android.app.DialogFragment;
+import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 
@@ -62,4 +64,54 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    private Intent setIntentExtra(int idActivity, Intent intent) {
+
+        switch (idActivity) {
+            case R.id.notes_quiz:
+                // do something
+                intent.putExtra("ButtonPressed", "notes_quiz");
+                break;
+            case R.id.chords_quiz:
+                intent.putExtra("ButtonPressed", "chord_quiz");
+
+                break;
+            case R.id.chord_progressions_quiz:
+                intent.putExtra("ButtonPressed", "chord_progressions_quiz");
+                break;
+        }
+
+          return intent;
+    }
+
+    public void goToGame(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, Game.class);
+
+       this.setIntentExtra(view.getId(),intent);
+
+        startActivity(intent);
+
+    }
+
+    public void goToStats(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, Stats.class);
+
+
+        startActivity(intent);
+
+    }
+
+    public void goToFaq(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, Faq.class);
+
+        startActivity(intent);
+
+    }
+
+
+
 }
